042e0f8b4c993a99d44f01d70f694b5756d33901
PJ Neill <pjneill51@hotmail.com>
2026-05-07 13:20:31 +0100

    Remove duplicate meme from ALZ post body

--- src/data/posts/alz-corp-online-local.tsx
+++ src/data/posts/alz-corp-online-local.tsx
@@ -21,10 +21,6 @@ export default function AlzCorpOnlineLocalContent() {
 
       <p style={p}>Corp. Short for corporate. &quot;Corporate&quot; just reminds me of Michael Scott from The Office 😂. It&apos;s an Americanism, really, usually interchangeable with &quot;company&quot; or &quot;organisation&quot; depending on where you&apos;re from.</p>
 
-      <div style={{ margin: '1.25rem 0' }}>
-        <img src="/alz-img-1.webp" alt="Michael Scott - I am so used to fighting corporate that I forget that I am corporate" style={{ width: '100%', borderRadius: '10px', display: 'block' }} />
-      </div>
-
       <p style={p}>But here we are. Corp exists, it&apos;s staying, and as of April 2026 it has a new sibling: the <strong>Local</strong> management group. Microsoft added it to the ALZ conceptual architecture. Worth understanding what they actually mean.</p>
 
       <h2 style={h2}>The Management Group Hierarchy</h2>
